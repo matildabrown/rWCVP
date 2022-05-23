4d011775fb295a77564c18d29915467708532385
barnabywalker <barnaby.walker08@gmail.com>
2022-05-23 16:31:05 +0100

    added test to make sure phonetic match gives same result as edit distance

--- tests/testthat/setup-match_data.R
+++ tests/testthat/setup-match_data.R
@@ -1,37 +1,46 @@
 library(dplyr)
 
 match_data <- tibble(
-  taxonId=c("taxon1", "taxon2", "taxon3", "taxon4", "taxon5", "taxon6"),
+  taxonId=c("taxon1", "taxon2", "taxon3", "taxon4", "taxon5", "taxon6", "taxon7"),
   scientificName=c("Poa annua", "Myrcia acuminatissima",
                    "Abutilon angulatum var. angulatum", "Mercia almasensis",
-                   "Myrcia almosensis", "× Tromostapelia mutabilis var americana"),
-  genus=c("Poa", "Myrcia", "Abutilon", "Mercia", "Myrcia", "Tromostapelia"),
-  species=c("annua", "acuminatissima", "angulatum", "almasensis", "almosensis", "mutabilis"),
-  infra_rank=c(NA_character_, NA_character_, "var.", NA_character_, NA_character_, "var"),
-  infra=c(NA_character_, NA_character_, "angulatum", NA_character_, NA_character_, "americana"),
-  authority=c("L.", "O.Berg", NA_character_, NA_character_, "NicLug", NA_character_)
+                   "Myrcia almosensis", "× Tromostapelia mutabilis var americana",
+                   "Astragalus canadense"),
+  genus=c("Poa", "Myrcia", "Abutilon", "Mercia", "Myrcia", "Tromostapelia",
+          "Astragalus"),
+  species=c("annua", "acuminatissima", "angulatum", "almasensis", "almosensis",
+            "mutabilis", "canadense"),
+  infra_rank=c(NA_character_, NA_character_, "var.", NA_character_, NA_character_,
+               "var", NA_character_),
+  infra=c(NA_character_, NA_character_, "angulatum", NA_character_, NA_character_,
+          "americana", NA_character_),
+  authority=c("L.", "O.Berg", NA_character_, NA_character_, "NicLug", NA_character_,
+              NA_character_)
 )
 
 lookup_data <- tibble(
   plant_name_id=c("435194-wcs", "130999-wcs", "130998-wcs", "534944-wcs",
-                  "1261494-az", "610452-az", "131016-wcs", "512990-wcs"),
+                  "1261494-az", "610452-az", "131016-wcs", "512990-wcs",
+                  "658189-az", "662570-az"),
   ipni_id=c("320035-2", "598579-1", "598578-1", "77199392-1", "77227834-1",
-            "1125427-2", "304073-2", "970028-1"),
+            "1125427-2", "304073-2", "970028-1", "24595-2", "479631-1"),
   taxon_name=c("Poa annua", "Myrcia acuminatissima", "Myrcia acuminatissima",
                "Myrcia acutissima", "Abutilon angulatum var. angulatum",
                "Abutilon angulatum", "Myrcia almasensis",
-               "× Tromostapelia mutabilis var. americana"),
+               "× Tromostapelia mutabilis var. americana", "Astragalus canadensis",
+               "Astragalus saguntinus"),
   taxon_rank=c("Species", "Species", "Species", "Species", "Variety", "Species",
-               "Species", "Variety"),
+               "Species", "Variety", "Species", "Species"),
   taxon_status=c("Accepted", "Unplaced", "Synonym", "Accepted", "Accepted",
-                 "Accepted", "Accepted", "Synonym"),
+                 "Accepted", "Accepted", "Synonym", "Accepted", "Invalid"),
   taxon_authors=c("L.", "Hieron.", "O.Berg", "(Urb.) K.Campbell & K.Samra",
-                  "", "(Guill. & Perr.) Mast.", "NicLugh.", "P.V.Heath"),
+                  "", "(Guill. & Perr.) Mast.", "NicLugh.", "P.V.Heath", "L.", "Pau"),
   family=c("Poaceae", "Myrtaceae", "Myrtaceae", "Myrtaceae", "Malvaceae",
-           "Malvaceae", "Myrtaceae", "Apocynaceae"),
+           "Malvaceae", "Myrtaceae", "Apocynaceae", "Fabaceae", "Fabaceae"),
   genus=c("Poa", "Myrcia", "Myrcia", "Myrcia", "Abutilon", "Abutilon", "Myrcia",
-          "Tromostapelia"),
+          "Tromostapelia", "Astragalus", "Astragalus"),
   accepted_plant_name_id=c("435194-wcs", NA_character_, "131542-wcs", "534944-wcs",
-                           "1261494-az", "610452-az", "131016-wcs", "512989-wcs"),
-  homotypic_synonym=c(NA, NA, NA, NA, NA, NA, NA, NA)
+                           "1261494-az", "610452-az", "131016-wcs", "512989-wcs",
+                           "658189-az", "662772-az"),
+  homotypic_synonym=c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
 )
