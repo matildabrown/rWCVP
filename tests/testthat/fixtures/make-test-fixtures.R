3d4de9d634cc13f139dfa2775f99c0dc4b49d9fb
Matilda Brown <M.Brown2@kew.org>
2023-04-24 11:06:06 +0100

    update tests for new wcvp_id structure

--- tests/testthat/fixtures/make-test-fixtures.R
+++ tests/testthat/fixtures/make-test-fixtures.R
@@ -55,48 +55,10 @@ match_data <- tibble(
 
 saveRDS(match_data, file = file.path(fixture_dir, "match_data.rds"))
 
-lookup_data <- tibble(
-  plant_name_id = c(
-    "435194-wcs", "130999-wcs", "130998-wcs", "534944-wcs",
-    "1261494-az", "610452-az", "131016-wcs", "512990-wcs",
-    "658189-az", "662570-az"
-  ),
-  ipni_id = c(
-    "320035-2", "598579-1", "598578-1", "77199392-1", "77227834-1",
-    "1125427-2", "304073-2", "970028-1", "24595-2", "479631-1"
-  ),
-  taxon_name = c(
-    "Poa annua", "Myrcia acuminatissima", "Myrcia acuminatissima",
-    "Myrcia acutissima", "Abutilon angulatum var. angulatum",
-    "Abutilon angulatum", "Myrcia almasensis",
-    "× Tromostapelia mutabilis var. americana", "Astragalus canadensis",
-    "Astragalus saguntinus"
-  ),
-  taxon_rank = c(
-    "Species", "Species", "Species", "Species", "Variety", "Species",
-    "Species", "Variety", "Species", "Species"
-  ),
-  taxon_status = c(
-    "Accepted", "Unplaced", "Synonym", "Accepted", "Accepted",
-    "Accepted", "Accepted", "Synonym", "Accepted", "Invalid"
-  ),
-  taxon_authors = c(
-    "L.", "Hieron.", "O.Berg", "(Urb.) K.Campbell & K.Samra",
-    "", "(Guill. & Perr.) Mast.", "NicLugh.", "P.V.Heath", "L.", "Pau"
-  ),
-  family = c(
-    "Poaceae", "Myrtaceae", "Myrtaceae", "Myrtaceae", "Malvaceae",
-    "Malvaceae", "Myrtaceae", "Apocynaceae", "Fabaceae", "Fabaceae"
-  ),
-  genus = c(
-    "Poa", "Myrcia", "Myrcia", "Myrcia", "Abutilon", "Abutilon", "Myrcia",
-    "Tromostapelia", "Astragalus", "Astragalus"
-  ),
-  accepted_plant_name_id = c(
-    "435194-wcs", NA_character_, "131542-wcs", "534944-wcs",
-    "1261494-az", "610452-az", "131016-wcs", "512989-wcs",
-    "658189-az", "662772-az"
-  ),
-  homotypic_synonym = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
-)
+
+lookup_data <- names %>% filter(taxon_name %in% lookup_data$taxon_name) %>%
+  select(plant_name_id, ipni_id, taxon_name, taxon_rank, taxon_status,
+         taxon_authors, family, genus, accepted_plant_name_id, homotypic_synonym) %>%
+  filter(taxon_name != "Poa annua" | taxon_authors != "Schltdl. & Cham.")
+
 saveRDS(lookup_data, file = file.path(fixture_dir, "lookup_data.rds"))
